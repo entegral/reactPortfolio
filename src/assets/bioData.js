2c1a7752d3c00b365aa288f069b50a58b25a247b
entegral <e2robby@gmail.com>
2019-02-22 10:44:54 -0800

    update source data with new project for displaying on homepage

--- src/assets/bioData.js
+++ src/assets/bioData.js
@@ -1,12 +1,25 @@
 const aboutMe = {
   title: 'About Me',
-  text: `Lorem ipsum dolor sit amet, facete appareat iracundia ut vim, ex dicit regione pro, reque vitae ei nec. Ei brute scripta per, an per verterem singulis accommodare. Timeam vulputate eam et. Repudiare instructior et vis, ad has agam admodum, no sea omnesque sensibus. At eloquentiam neglegentur sit, his et omnium delectus. An nobis audiam accusamus vim, eam in deleniti scaevola molestiae.
-
-  Et nostro audiam mel, eos malis tation commune ei.Ei alterum mentitum eos, ea elit alterum scaevola ius.Tollit verterem abhorreant no eam, ferri aliquando referrentur ex qui.Sea ne ocurreret reprehendunt.Posse dictas interpretaris mel ex, vidit legimus cu his, dolorum accusam detraxit no mei.Mei augue scripta ea.
-
-  Id dicit fabulas neglegentur sed.Euismod mnesarchum no vix, mei errem labitur adipiscing et.Eam sale commune no, sed ad augue causae.In nam dolores percipitur, an augue temporibus mel.Tation ridens meliore cum at, quo ex luptatum apeirian eleifend.
-
-  Ne oblique virtute saperet his.Pro at recusabo petentium intellegam, vis no minim tractatos, ex veniam labores sensibus vis.Consul nusquam consectetuer sed ex, justo luptatum eu mel.No has sale integre, dicta voluptaria quo eu.Eu aperiam corpora mediocrem mel, ne ubique reprimique deterruisset sea, eu eos illud fabulas detracto.`
+  text: 'In October of 2018 I decided to professionalize a career in Tech by attending the Ruby/Rails/React course at Epicodus. My decision to professionally transition myself out of Manufacturing and into the Tech industry was rooted in the acknowledgment of a lifelong passion and fascination in software. Somewhere between building media servers for friends, building/operating an array of cryptocurrency mining rigs, and writing software for Burning Man I realized that I needed to support my inherent passion for software development and enroll in a program that would channel this interest in the most effective, fulfilling and professional way. Tech is my passion, and I look forward to participating in this incredibly exciting industry.'
 };
 
-export default aboutMe;
+
+const projects = [
+  {
+    title: 'keyCatch',
+    description: 'This app was the first to see real world adoption and use. It is a Python/Django app and was developed to solve a specific issue that plagued my team of volunteers at the annual Burning Man Event. Prior to its implementation, shift keys were transferred from the outgoing shift to the incoming shift via a mutually agreed-upon hiding location. This caused many problems with lost and misplaced shift keys, frequently resulting in delays to critical aspects of our customer\'s operation. keyCatch replaced the manual hand-off process through the use of a large electronic locker/dropbox system that logs key removal and deposit events for a given set of users. This system has reduced lost-key events by more than 50% since its initial introduction and has become an integral part of the event\'s operation.',
+    link: ''
+  },
+  {
+    title: '',
+    description: '',
+    link: ''
+  },
+  {
+    title: '',
+    description: '',
+    link: ''
+  },
+];
+
+export default {projects, aboutMe};
